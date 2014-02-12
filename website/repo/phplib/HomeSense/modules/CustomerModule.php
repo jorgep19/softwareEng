c07a0cdc7a793686ee166e7142f1c10592f2af74
Andrei Sura <indera@gmail.com>
2014-02-11 23:52:22 -0500

    adding skeleton files `CustomerModule, DeviceModule`

---
+++ website/repo/phplib/HomeSense/modules/CustomerModule.php
@@ -0,0 +1,21 @@
+<?php
+/**
+*  Home Sense CustomerModule class
+*
+*  Handles requests related to a Raspberry Pi sensor
+*  @author Andrei Sura 
+*/
+
+class CustomerModule {
+
+   public function validateAction() {
+      $errors = array();
+
+      echo "\n CustomerModule::validateAction()";
+      return $errors;
+
+   } 
+
+}
+
+
