0a5785429b8f14371bc65677e4d84bc886b1372b
jorgep19 <jorgelpaez19@gmail.com>
2014-02-23 20:29:47 -0500

    posting to create user

--- website/repo/views/clientLogic/HomePageVM.js
+++ website/repo/views/clientLogic/HomePageVM.js
@@ -10,16 +10,34 @@ function LoginVM() {
 
 LoginVM.prototype.doLogin = function(){
     var self = this;
+
     // send ajax to login URL
     // and go to pages/dashboard.html"
-    alert("login as username: "+self.userName()+" password: "+self.password());
+    /*$.get(
+        "loginURL.php",
+        "{username:"+self.userName+",password"+self.password+"}",
+        function(data) {
+            if(data.loginSuccess){
+                window.location = "./pages/dashboard.html?u="+self.userName();
+            }
+            else{
+                alert("login error!");
+            }
+        },
+        "html"
+    );*/
 };
 
 LoginVM.prototype.createAccount = function() {
     var self = this;
 
-    alert("creating account as username: "+self.userName()+" email: "+self.email()+" password: "
-        +self.password()+" password-retype: "+self.passwordRetype());
+    $.ajax({
+        url: "http://homesense.abovotec.com/customer/register?cusEmail="+self.email()+"&cusFrist=x&cusLast=y&cusMI=z"
+    }).done(function( data ) {
+            if ( console && console.log ) {
+                console.log( "Sample of data:", data.slice( 0, 100 ) );
+            }
+        });
 };
 
 ko.applyBindings(new LoginVM());
