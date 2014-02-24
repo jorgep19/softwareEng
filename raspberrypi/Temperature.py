21bdc6381bf2ca20491c9757b736ca2cc662cd1d
lucasarango <lucasarango@ufl.edu>
2014-02-23 23:20:58 -0500

    Update Temperature.py

--- raspberrypi/Temperature.py
+++ raspberrypi/Temperature.py
@@ -2,6 +2,8 @@ import urllib.parse
 import urllib.request
 import time
 import json
+import random
+
 
 timestamp = time.strftime("%Y-%m-%d-%H-%M-%S")
 
@@ -12,9 +14,12 @@ temperature_data = text.split()[-1]
 temperature = float(temperature_data[2:])
 temperature = temperature / 1000
 
-jdata= {"email":"indera@gmail.com","sensor":"TempSens1","value":59,"date":timestamp,"device":"RasPi_Garage"}
-data = urllib.parse.urlencode(jdata)
-binary_data = data.encode('ascii')
-req =urllib.request.Request("http://www.homesense.abovotec.com/api/sensor/put_data/?", binary_data)
-
-urllib.request.urlopen(req)
+i=0
+while(i<20):
+    jdata= {"email":"indera@gmail.com","sensor":"TempSens1","value":temp,"date":timestamp,"device":"RasPi_Garage"}
+    data = urllib.parse.urlencode(jdata)
+    binary_data = data.encode('ascii')
+    req =urllib.request.Request("http://www.homesense.abovotec.com/api/sensor/put_data/?", binary_data)
+    urllib.request.urlopen(req)
+    i+=1
+    time.sleep(3) # delays for 5 seconds
