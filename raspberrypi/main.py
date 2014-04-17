3e7a26ec7fd5648acaef59db16eaa0aef1ec6a30
lucasarango <lucasarango@ufl.edu>
2014-04-17 19:26:27 -0400

    Update main.py

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -30,7 +30,7 @@ url = "http://homesense.herokuapp.com/api/sensor/put/data/"
 sensors_data=[]
 for sensor in sensors:
         print(sensor)
-        sensors_data.append({'sensId': sensor['sensid'], 'value': 12,'date':timestamp})
+        sensors_data.append({'sensId': sensor['sensid'], 'value': getTemperature(),'date':timestamp})
         print(sensors_data)
 
 
@@ -39,6 +39,14 @@ sent_data = {'userId':user['userId'], 'sensors':sensors_data}
 print(sent_data)
 response = requests.post(url, data=json.dumps(sent_data),headers=headers)
 print(response.text)
-time.sleep(60)
+
+def getTemperature():
+	tfile = open("/sys/bus/w1/devices/w1_bus_master1/10-000802824e58/w1_slave")
+	text = tfile.read()
+	tfile.close()
+	temperature_data = text.split()[-1]
+	temperature = float(temperature_data[2:])
+	temperature = temperature / 1000 *9.0 /5.0 +32
+	return temperature
 
 
