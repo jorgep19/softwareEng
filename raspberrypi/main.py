51a3b5a77818b67410ca58107ef8573b15bae80d
lucasarango <lucasarango@ufl.edu>
2014-04-18 00:07:51 -0400

    Update main.py

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -11,7 +11,16 @@ GPIO.setmode(GPIO.BCM)
 def motion(channel): 
         sendEmail(); 
         print ("MOTION DETECTED")
-    
+ 
+def getTemperature():
+	tfile = open("/sys/bus/w1/devices/w1_bus_master1/10-000802824e58/w1_slave")
+	text = tfile.read()
+	tfile.close()
+	temperature_data = text.split()[-1]
+	temperature = float(temperature_data[2:])
+	temperature = temperature / 1000 *9.0 /5.0 +32
+	return temperature
+	
 # PIN SETUP
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(4, GPIO.IN) 
@@ -40,13 +49,6 @@ print(sent_data)
 response = requests.post(url, data=json.dumps(sent_data),headers=headers)
 print(response.text)
 
-def getTemperature():
-	tfile = open("/sys/bus/w1/devices/w1_bus_master1/10-000802824e58/w1_slave")
-	text = tfile.read()
-	tfile.close()
-	temperature_data = text.split()[-1]
-	temperature = float(temperature_data[2:])
-	temperature = temperature / 1000 *9.0 /5.0 +32
-	return temperature
+
 
 
